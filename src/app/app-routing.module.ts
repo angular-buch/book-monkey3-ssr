274a568c54eb8f66163eca762428df48114f0453
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-05-27 15:03:49 +0200

    Initial BookMonkey3 without SSR

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { CanNavigateToAdminGuard } from './can-navigate-to-admin.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'books',
+    loadChildren: './books/books.module#BooksModule'
+  },
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule',
+    canActivate: [CanNavigateToAdminGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
